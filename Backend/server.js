084e12682678cb73d1f06306142cb96ca0524246
kian-drego <drego.kian.boi@gmail.com>
2026-02-11 03:24:51 +0530

    1

--- Backend/server.js
+++ Backend/server.js
@@ -18,3 +18,10 @@ app.listen(5000, () => console.log("Server running on port 5000"));
 app.get('/', (req, res) => {
     res.send("<h1>PantryPal API is Live!</h1><p>The backend is working perfectly.</p>");
 });
+// Add these to server.js
+const authRoutes = require('./routes/auth');
+const recipeRoutes = require('./routes/recipes');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/recipes', recipeRoutes);
+	
